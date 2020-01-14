b6d12a2d9ec4a7c2a44534c49be6112dda1479b4
tikano <oadmathgenie@gmail.com>
2020-01-13 19:48:04 -0500

    Update exercises.js

--- src/exercises.js
+++ src/exercises.js
@@ -137,6 +137,7 @@ function consecutive(numbers) {
 
 function balance(numbers) {
   function sum(array){
+    var sum = 0;
     for(var i = 0; i < array.length; i++){
       sum = sum + array[i];
     }
